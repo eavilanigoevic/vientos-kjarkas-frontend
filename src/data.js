17403817193c806248980d27a6411fd3599c74ee
eavilanigoevic <eavila_nigoevic@hotmail.com>
2022-06-18 20:09:48 -0400

    first commit

---
+++ src/data.js
@@ -0,0 +1,63 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'Quena',
+      slug: 'quenas-quena-bambu',
+      category: 'quenas',
+      image: '/images/vk1.jpg', //679px x 829px
+      description:
+        'Quena profesional de Bambú. Disponible en diferentes afinaciones',
+      price: 60,
+    },
+    {
+      _id: '2',
+      name: 'Juego de zampoñas en afinación Do',
+      slug: 'zamponias-afinacion-do',
+      category: 'zamponias',
+      image: '/images/vk2.jpg',
+      description:
+        'Juego de zampoñas en afinación Do profesional. (Zanka, Malta e ika)',
+      price: 170,
+    },
+    {
+      _id: '3',
+      name: 'Malta Cromática de tres filas',
+      slug: 'zamponias-malta-cromatica',
+      category: 'zamponias',
+      image: '/images/vk3.jpg',
+      description:
+        'Disponible en diferentes colores de amarre (rojo, azul, negro)',
+      price: 100,
+    },
+    {
+      _id: '4',
+      name: 'Zampoña Zanka en afinación Mi menor',
+      slug: 'zamponias-zanka-mi',
+      category: 'zamponias',
+      image: '/images/vk4.jpg',
+      description: 'Bambú de espesor grueso',
+      price: 70,
+    },
+    {
+      _id: '5',
+      name: 'Zampoña Malta en afinación Mi menor',
+      slug: 'zamponias-malta-mi',
+      category: 'zamponias',
+      image: '/images/vk5.jpg',
+      description: 'Producto recomendado para principiantes ',
+      price: 50,
+    },
+    {
+      _id: '6',
+      name: 'Juego de zampoñas en afinación Mi menor',
+      slug: 'zamponias-juego-mi',
+      category: 'zamponias',
+      image: '/images/vk6.jpg',
+      description: 'Zanka, Malta e Ika. Bambú de espesor grueso',
+      price: 150,
+    },
+  ],
+};
+
+export default data;
